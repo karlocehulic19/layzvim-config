b0adde984de46a8d2d8a251f073aa6c677da485e
Karlo Cehulic <karlocehlic@gmail.com>
2026-02-11 16:38:45 +0100

    Add jdtls lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,11 +2,17 @@ return {
   {
     "mason-org/mason-lspconfig.nvim",
     opts = {
+      -- automatic_enable = {
+      --   exclude = { "jdtls" },
+      -- },
       ensure_installed = { "ts_ls" },
     },
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
       "neovim/nvim-lspconfig",
     },
+  },
+  {
+    "mfussenegger/nvim-jdtls",
   },
 }
